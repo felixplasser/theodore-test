e4f69ba21c51d532b28c9771a65d6d3b8ad88dad
Felix Plasser <F.Plasser@lboro.ac.uk>
2021-12-21 16:20:46 +0000

    util tests

---
+++ UTILS/fa2.col-NOs/test_fa2-NOs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from theodore import lib_pytest
+
+def test_utils():
+    pjob = lib_pytest.pytest_job(__file__)
+    
+    pjob.run_util('analyze_nos molden_no_ci.drt1.state1.sp molden_no_ci.drt1.state2.sp molden_no_ci.drt1.state3.sp')
+
+    stdin = open('../IN_FILES/jmol_mos.in', 'r').read()
+    pjob.run_util('jmol_mos -f molden_no_ci.drt1.state1.sp molden_no_ci.drt1.state2.sp molden_no_ci.drt1.state3.sp', stdin)
+    
+    pjob.check()
--- UTILS/pv2p.qctddft/test_pv2p-util.py
+++ UTILS/pv2p.qctddft/test_pv2p-util.py
@@ -3,4 +3,18 @@
 from theodore import lib_pytest
 
 def test_standard():
-    lib_pytest.pytest_job(__file__).run_utils()
+    pjob = lib_pytest.pytest_job(__file__)
+    
+    stdin = open('../IN_FILES/plot_omfrag.in', 'r').read()
+    pjob.run_util('plot_omfrag', stdin)
+
+    stdin = open('../IN_FILES/plot_om_bars.in', 'r').read()
+    pjob.run_util('plot_om_bars', stdin)
+
+    stdin = open('../IN_FILES/plot_frag_decomp.in', 'r').read()
+    pjob.run_util('plot_frag_decomp', stdin)
+
+    stdin = open('../IN_FILES/spectrum.in', 'r').read()
+    pjob.run_util('spectrum tden_summ.txt tden_summ2.txt', stdin)
+
+    pjob.check()
