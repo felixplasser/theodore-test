99315985258013c24aa7f1bbc8d9f5a3b029517d
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2022-04-27 10:34:54 +0100

    add test for jmol dens

--- UTILS/fa2.col-NOs/test_fa2-NOs.py
+++ UTILS/fa2.col-NOs/test_fa2-NOs.py
@@ -10,6 +10,9 @@ def test_utils():
     stdin = open('../IN_FILES/jmol_mos.in', 'r').read()
     pjob.run_util('jmol_mos -f molden_no_ci.drt1.state1.sp molden_no_ci.drt1.state2.sp molden_no_ci.drt1.state3.sp', stdin)
 
+    stdin = open('../IN_FILES/jmol_dens.in', 'r').read()
+    pjob.run_util('jmol_mos -f molden_no_ci.drt1.state2.sp', stdin)
+
     stdin = open('../IN_FILES/dgrid_prep.in', 'r').read()
     pjob.run_util('dgrid_prep molden_no_ci.drt1.state1.sp molden_no_ci.drt1.state2.sp molden_no_ci.drt1.state3.sp', stdin)
 
